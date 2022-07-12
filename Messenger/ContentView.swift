2deef426d434537670c26e61d9bd15c64ae74452
Selena Mead <selenaelizabethmead@gmail.com>
2022-07-12 18:08:23 -0400

    Initial Commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Selena Mead on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Selena Mead on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
